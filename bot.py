a661b8e72e32060ef5a0f491c710b671eb9cf99a
Alton-byte-sv1 <chicky090909@gmail.com>
2025-08-24 15:24:35 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,27 +1,10 @@
 import requests
 import os
 
-TOKEN = os.getenv("8359549328:AAEOIyIPuwl3eE_qhHZR_qWjC5tmKEEtcZs")
-CHAT_ID = os.getenv("@faratechinc")
+TOKEN = os.getenv("TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
 
-def get_quote():
-    try:
-        r = requests.get("https://zenquotes.io/api/random")
-        data = r.json()
-        return f"{data[0]['q']} — {data[0]['a']}"
-    except Exception:
-        return "Keep pushing forward 💪"
-
-def send_message(text):
-    url = f"https://api.telegram.org/bot{8359549328:AAEOIyIPuwl3eE_qhHZR_qWjC5tmKEEtcZs}/sendMessage"
-    payload = {"chat_id": CHAT_ID, "text": text}
-    requests.post(url, data=payload)
-
-if __name__ == "__main__":
-    quote = get_quote()
-    send_message(quote)
-    print("Sent:", quote)
-
-
-
-
+url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+payload = {"chat_id": CHAT_ID, "text": "🚀 Test message from GitHub Actions!"}
+response = requests.post(url, data=payload)
+print(response.text)
