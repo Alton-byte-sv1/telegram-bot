940f7b7dacd9ade58750cd5f664ed5c92081884e
Alton-byte-sv1 <chicky090909@gmail.com>
2025-08-24 14:46:49 +0100

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,23 @@
+import requests
+import os
+
+TOKEN = os.getenv("8359549328:AAEOIyIPuwl3eE_qhHZR_qWjC5tmKEEtcZs")
+CHAT_ID = os.getenv("-1002335264449")
+
+def get_quote():
+    try:
+        r = requests.get("https://zenquotes.io/api/random")
+        data = r.json()
+        return f"{data[0]['q']} — {data[0]['a']}"
+    except Exception:
+        return "Keep pushing forward 💪"
+
+def send_message(text):
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+    payload = {"chat_id": CHAT_ID, "text": text}
+    requests.post(url, data=payload)
+
+if __name__ == "__main__":
+    quote = get_quote()
+    send_message(quote)
+    print("Sent:", quote)
